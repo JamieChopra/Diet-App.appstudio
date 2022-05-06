e92477ba711afc824ddf8704cc9df55ee9ad6f95
JamieChopra <jwc258@hotmail.co.uk>
2022-05-06 08:43:59 +0100

    Working on delete button using checkboxes

--- Diet App.appstudio/forms/Meal/Meal.js
+++ Diet App.appstudio/forms/Meal/Meal.js
@@ -19,8 +19,13 @@ Button1.onclick=function(){
   //Add the last item of the array to the list
   Listgroup1.addItem(listMeal[(listMeal.length - 1)]);
   
+  Checkbox1.addItem("Delete");
+  
+  console.log(Listgroup1);
+  
   let sum = 0;
   
+  // Add all of the meal inputs together
   for (let x = 0; x < listMeal.length; x++){
         sum += parseInt(listMeal[x]);
     }
@@ -38,6 +43,16 @@ Button1.onclick=function(){
   // Create method for turning progress bar red if 
   // it goes over 100%
 
+  
+
+}
+
+
+Button3.onclick=function(){
+  let checked = Checkbox1.length - 1;
+  console.log(checked);
+  delete Listgroup1[checked];
+  console.log(checked);
 }
 
 
