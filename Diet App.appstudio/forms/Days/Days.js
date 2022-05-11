a55e991875a2792167534dd5f8d5055ca55c42af
JamieChopra <JamieChopra@users.noreply.github.com>
2022-05-11 21:38:46 +0100

    Improving UI

--- Diet App.appstudio/forms/Days/Days.js
+++ Diet App.appstudio/forms/Days/Days.js
@@ -1,6 +1,6 @@
 //Function to delete the first item in the list
 
-Button4.onclick=function(){
+DeleteDay.onclick=function(){
   CaloriesList.removeChild(CaloriesList.firstElementChild);
   DateList.removeChild(DateList.firstElementChild);
   HitGoalList.removeChild(HitGoalList.firstElementChild);
